7a80a3671c500f98cac4c4bf75f26276000ef4c2
George Koroulis <gkoroulis7201@stu.d214.org>
2025-04-10 10:51:06 -0500

    Created answer variable and text to display the answer variable

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,13 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer = 0
     var body: some View {
         
         TextField("", value: $number1, formatter: NumberFormatter())
         
         TextField("Enter A Number", value: $number2, formatter: NumberFormatter())
+        
+        Text("The answer is \(answer)")
     }
 }
