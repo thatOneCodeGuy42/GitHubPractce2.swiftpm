d96892a5bbe712d30be8cba5415c10fb83c4dfbd
George Koroulis <gkoroulis7201@stu.d214.org>
2025-04-10 10:47:05 -0500

    Added a textfield connected to number2

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,6 @@ struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Enter A Number", value: $number2, formatter: NumberFormatter())
     }
 }
